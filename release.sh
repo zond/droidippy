9ee2e1008617f5fd0c22093f2360811a4aefc5f0
Martin Bruse <zondolfin@gmail.com>
2014-12-21 01:05:31 +0100

    new signature code

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ant -Dbase.port=${PORT:-443} -Dbase.domain=${SERVER:-droidippy.oort.se} clean release && \
-jarsigner -verbose -keystore zond.keystore bin/Droidippy-release-unsigned.apk mykey && \
-jarsigner -verify bin/Droidippy-release-unsigned.apk && \
+jarsigner -verbose -sigalg SHA1withDSA -digestalg SHA1 -keystore zond.keystore bin/Droidippy-release-unsigned.apk mykey && \
+jarsigner -verify -verbose -certs bin/Droidippy-release-unsigned.apk && \
 zipalign -v 4 bin/Droidippy-release-unsigned.apk bin/Droidippy.apk
 
